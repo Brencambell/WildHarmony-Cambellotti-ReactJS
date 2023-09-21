5d70d94919e54af5ab5027b19e7c9141a84a03e0
Brenda Cambellotti <brencambellotti@gmail.com>
2023-09-21 19:12:33 +0200

    Context part one

--- wild-harmony/src/components/CartWidget/CartWidget.jsx
+++ wild-harmony/src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,16 @@
+import { useContext } from 'react';
+import CartContext from '../../context/CartContext';
+
 const CartWidget = () => {
+    const  { cart } = useContext(CartContext);
+
     return (
         <div>
             <button className="btn btn-outline-danger position-relative">
                 <i className="bi bi-cart"></i>
                 <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                    97
-                     <span className="visually-hidden">Productos en el carrito</span>
+                    {cart.lenght}
+                    <span className="visually-hidden">Productos en el carrito</span>
                  </span>
             </button>
         </div>
